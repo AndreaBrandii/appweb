9746cb0ad6b7ae26eb73cf9d87eba0dea5dec2f6
AndreaBrandii <68716296+AndreaBrandii@users.noreply.github.com>
2023-09-21 15:49:23 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página de Prueba</title>
+</head>
+<body>
+    <h1>Welcome!!</h1>
+    
+    <p>La fecha y hora actual es: <?php echo date("Y-m-d H:i:s"); ?></p>
+</body>
+</html>
